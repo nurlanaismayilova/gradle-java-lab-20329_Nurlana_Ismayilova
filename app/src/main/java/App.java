6e9aaea3665432b87bc7a36cb9ce3ee8286e2ee0
LemanSalmanli <lemansalmanli15@gmail.com>
2026-02-03 00:37:40 +0400

    Updated message by student B

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -5,7 +5,7 @@
 
 public class App {
     public String getGreeting() {
-        return "Gradle build and Git workflow successful.";
+        return "Updated message by Student B";
     }
 
     public static void main(String[] args) {
